c06e645c542607441cfc47f3288b35836361a229
Jon Sundin <jsundin91@gmail.com>
2022-07-31 15:32:37 -0700

    deleted comments that are best for dev branch

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-/*
-background.js is the service worker for this Chrome extension
-it is a separate process from the web page, and thus does not have access
-to the web page (the DOM)
-background.js can communicate with the extension through the Chrome messaging API
-*/
-/* global variable declarations and assignments */
-
 const highlightColorChoices = [
   "yellow",
   "red",
@@ -16,26 +8,15 @@ const highlightColorChoices = [
 ];
 const actions = { highlightSelectedText: "highlight-selected-text" };
 
-/* end of global variable declarations */
 
 /* ----------- getter functions ----------------- */
 
-/*
-get current tab
-function declarations are hoisted by JavaScript at runtime
-the active tab is the one selected in a window
-lastFocusedWindow is the window that is being viewed (the one 'on top' of other application windows)
-*/
 function getActiveTab() {
   let queryOptions = { lastFocusedWindow: true, active: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`
-  let tabQueryPromise = chrome.tabs.query(queryOptions); // chrome.tabs.query returns a Promise object
+  let tabQueryPromise = chrome.tabs.query(queryOptions);
   tabQueryPromise
     .then((activeTab) => {
       if (activeTab) {
-        console.log(
-          "getActiveTab: activeTab JSON string: " + JSON.stringify(activeTab)
-        );
         return activeTab;
       }
     })
@@ -45,29 +26,12 @@ function getActiveTab() {
 }
 
 /* ----------- end of getter functions ---------- */
-/*
-  for YouTube video search parameters
- const queryParameters = tab.url.split("?")[1];
- const urlParameters = URLSearchParams(queryParameters);
 
 
-  currentWindow is that which code is executing
-  if the currentWindow created many tabs or windows from a single HTML file,
-  the currentWindow will be the one that the original window where that HTML file
-  made the call to tabs.query
-
-  The currentWindow can be different than the top-most or focused window
-  if the initial call created other windows that become the top-most or focused window
-  the queryOptions below will only retrieve one tab due to the two constraints
-  per window, only one tab will ever be `active`
-
-  */
 /* message functions */
 
-// for single messages use the below Chrome messaging API
+
 function sendOneTimeMessage(message) {
-  // chrome.tabs.sendMessage required parameters: tabId, message
-  // optional parameters: options, callback func
   if (message.context === "onUpdatedTab") {
     chrome.tabs.sendMessage(message.info.tabId, message.info);
   }
@@ -76,7 +40,6 @@ function sendOneTimeMessage(message) {
   }
 }
 
-// long-lived messaging connection
 function runPortMessagingConnection(obj) {
   let activeTab = getActiveTab();
   if (activeTab && activeTab.length > 0) {
@@ -86,10 +49,6 @@ function runPortMessagingConnection(obj) {
     port.onDisconnect = (err) => {
       console.error("disconnected", err);
     };
-
-    /*port.onMessage.addListener((response) => {
-      console.error('port.onMessage response=',response);
-    });*/
   }
 }
 
@@ -99,7 +58,7 @@ function runPortMessagingConnection(obj) {
 
 function onUpdatedTab(tabId, changeInfo, tab) {
   if (tab) {
-    // console.log("onUpdatedTab: tabObj: " + JSON.stringify(tabObj));
+    
     let message = {
       context: "onUpdatedTab",
       info: {
@@ -115,14 +74,6 @@ function onUpdatedTab(tabId, changeInfo, tab) {
   }
 }
 
-/* info object passed when contextMenu item is clicked
-  callback parameters include: (info: OnClickData, tab?: tabs.Tab)
-  info = { // of these the menuItemId is required
-      menuItemId: string or int: the ID of the menu item that was clicked,
-      pageUrl: string: The URL of the page where the menu item was clicked. This property is not set if the click occured in a context where there is no current page, such as in a launcher context menu.,
-      selectionText: string: The text for the context selection, if any.,
-      srcUrl: string: Will be present for elements with a 'src' URL.
-    } */
 function onClickContextMenus(info, tab) {
   if (highlightColorChoices.includes(info.menuItemId)) {
     let message = {
@@ -140,19 +91,15 @@ function onClickContextMenus(info, tab) {
   }
 }
 
-// callback function for context menu create functions
-// if error occured, chrome.runtime.lastError will include it
 function contextMenusCreateCallback() {
   console.log("context menu create error: " + chrome.runtime.lastError);
 }
 
 function createContextMenus() {
-  // context menu create takes two parameters (menuItemProperties, callbackHandler)
-  // these menu items will only show in the context menu with the context of selection of text
   chrome.contextMenus.create(
     {
       id: "yellow",
-      title: "yellow", // (Ctrl-Shift-Y)",
+      title: "yellow",
       type: "normal",
       contexts: ["selection"],
     },
@@ -162,7 +109,7 @@ function createContextMenus() {
   chrome.contextMenus.create(
     {
       id: "red",
-      title: "red", // (Ctrl-Shift-Y)",
+      title: "red",
       type: "normal",
       contexts: ["selection"],
     },
@@ -172,7 +119,7 @@ function createContextMenus() {
   chrome.contextMenus.create(
     {
       id: "grey",
-      title: "grey", // (Ctrl-Shift-Y)",
+      title: "grey",
       type: "normal",
       contexts: ["selection"],
     },
@@ -184,18 +131,8 @@ function createContextMenus() {
 
 /* listeners */
 
-// chrome.tabs.onActivated.addListener(activatedTabHandler);
-
-// listening for a new tab opened or to a new URL typed in a tab
 chrome.tabs.onUpdated.addListener(onUpdatedTab);
-
-// on drag end listener
-// chrome.event.addListener('dragend', onDragEventHandler);
-
-// context menu listener
-// callback parameters include: (info: OnClickData, tab?: tabs.Tab)
 chrome.contextMenus.onClicked.addListener(onClickContextMenus);
-
 chrome.runtime.onInstalled.addListener(createContextMenus);
 
 /* end: listeners */
