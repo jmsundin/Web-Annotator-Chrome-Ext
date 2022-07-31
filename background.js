338e1c99b86495c92ba38ae7cf5d39d3611e5477
Jon Sundin <jsundin91@gmail.com>
2022-07-31 09:55:16 -0700

    updated getActiveTab func, created two messaging funcs for single and port messaging cleaned up comments

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
-// background.js is the service worker for this Chrome extension
-// it is a separate process from the web page, and thus does not have access
-// to the web page (the DOM)
-// background.js can communicate with the extension through the Chrome messaging API
-
+/*
+background.js is the service worker for this Chrome extension
+it is a separate process from the web page, and thus does not have access
+to the web page (the DOM)
+background.js can communicate with the extension through the Chrome messaging API
+*/
 /* global variable declarations and assignments */
 
 const highlightColorChoices = [
@@ -19,68 +20,68 @@ const actions = {highlightSelectedText: "highlight-selected-text"};
 
 /* ----------- getter functions ----------------- */
 
-// get current tab
-// function declarations are hoisted by JavaScript at runtime
-
-// function getActiveTab() {
-//   // the active tab is the one selected in a window
-//   // lastFocusedWindow is the window that is being viewed (the one 'on top' of other application windows)
-//   let queryOptions = {lastFocusedWindow: true, active: true };
-//   // `tab` will either be a `tabs.Tab` instance or `undefined`
-//   let tabQueryPromise = chrome.tabs.query(queryOptions);  // chrome.tabs.query returns a Promise object
-//   tabQueryPromise.then((tab) => {
-//       if (tab){
-//         console.log("in getActiveTab: activeTab: " + tab);
-//       }
-//     })
-//     .catch(err => {
-//       console.log("in getActiveTab: error: " + err)
-//     });
-
-//   if (activeTab) {
-//   console.log("inside getCurrentTab: " + activeTab.url);
-//   return activeTab;
-// }
-// }
+/*
+get current tab
+function declarations are hoisted by JavaScript at runtime
+the active tab is the one selected in a window
+lastFocusedWindow is the window that is being viewed (the one 'on top' of other application windows)
+*/
+function getActiveTab() {
+  let queryOptions = {lastFocusedWindow: true, active: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`
+  let tabQueryPromise = chrome.tabs.query(queryOptions);  // chrome.tabs.query returns a Promise object
+  tabQueryPromise.then((activeTab) => {
+      if (activeTab){
+        console.log("in getActiveTab: activeTab JSON string: " + JSON.stringify(activeTab));
+        return activeTab;
+      }
+    })
+    .catch(err => {
+      console.log("in getActiveTab: error: " + err)
+    });
+}
 
 /* ----------- end of getter functions ---------- */
+/*
+  for YouTube video search parameters
+ const queryParameters = tab.url.split("?")[1];
+ const urlParameters = URLSearchParams(queryParameters);
 
-// for YouTube video search parameters
-// const queryParameters = tab.url.split("?")[1];
-// const urlParameters = URLSearchParams(queryParameters);
 
-/* message functions */
+  currentWindow is that which code is executing
+  if the currentWindow created many tabs or windows from a single HTML file,
+  the currentWindow will be the one that the original window where that HTML file
+  made the call to tabs.query
 
-// currentWindow is that which code is executing
-  // if the currentWindow created many tabs or windows from a single HTML file,
-  // the currentWindow will be the one that the original window where that HTML file
-  // made the call to tabs.query
+  The currentWindow can be different than the top-most or focused window
+  if the initial call created other windows that become the top-most or focused window
+  the queryOptions below will only retrieve one tab due to the two constraints
+  per window, only one tab will ever be `active`
 
-  // The currentWindow can be different than the top-most or focused window
-  // if the initial call created other windows that become the top-most or focused window
-  // the queryOptions below will only retrieve one tab due to the two constraints
-  // per window, only one tab will ever be `active`
-function sendMessageActiveTab(obj) {
-  const queryOptions = { active: true, currentWindow: true };
-  chrome.tabs.query(queryOptions, (tab) => {
-    if (tab && tab.length > 0) {
-      // for single messages use the below Chrome messaging API
-      //chrome.tabs.sendMessage(tab.id, json, function(response) {});
+  */
+  /* message functions */
 
-      // for longer messaging, open a messaging port
-      console.log("inside sendMessageActiveTab: tabUrl: " + tab.url);
-      const port = chrome.tabs.connect(tab.id);
-      port.postMessage(obj);
-      port.onDisconnect = (err) => {
-        console.error("disconnected", err);
-      };
-
-      /*port.onMessage.addListener((response) => {
-        console.error('port.onMessage response=',response);
-      });*/
-    }
-  });
+function sendOneTimeMessage(obj){
+  // for single messages use the below Chrome messaging API
+  chrome.tabs.sendMessage(obj, callbackFunc);
 }
+
+// long-lived messaging connection
+function runPortMessagingConnection(obj) {
+  let activeTab = getActiveTab();
+  if (activeTab && activeTab.length > 0) {
+    console.log("inside sendMessageActiveTab: tabUrl: " + tab.url);
+    const port = chrome.tabs.connect(tab.id);
+    port.postMessage(obj);
+    port.onDisconnect = (err) => {
+      console.error("disconnected", err);
+    };
+
+    /*port.onMessage.addListener((response) => {
+      console.error('port.onMessage response=',response);
+    });*/
+  }
+};
 
 /* end: message functions */
 
@@ -90,12 +91,11 @@ function activatedTabHandler(activeTabInfo) {
   console.log(`activeTabId: ${activeTabInfo.tabId}`);
 }
 
-function onUpdatedTabHandler(tabId, changeInfo, tabInfo) {
+function onUpdatedTabHandler(tabId, changeInfo, tab) {
   console.log("Updated tab: " + tabId);
-  console.log("Changed attributes: ");
-  console.log(changeInfo);
-  console.log("New tab Info: ");
-  console.log(tabInfo);
+  console.log("tab object from chrome api: " + tab);
+  console.log("Updated tab url: " + tab.url);
+  console.log("changedInfo for tab: " + changeInfo);
 }
 
 function onDragEventHandler(event){
@@ -114,7 +114,7 @@ function onDragEventHandler(event){
 function contextMenusHandler(info, tab) {
   console.log("contextMenuesHandler: menuItemId: " + info.menuItemId);
   if (highlightColorChoices.includes(info.menuItemId)) {
-    sendMessageActiveTab({
+    sendOneTimeMessage({
       action: actions.highlightSelectedText,
       highlightColor: info.menuItemId,
       tabUrl: tab.url,
@@ -133,7 +133,7 @@ function contextMenusCreateCallbackHandler() {
 
 /* listeners */
 
-chrome.tabs.onActivated.addListener(activatedTabHandler);
+// chrome.tabs.onActivated.addListener(activatedTabHandler);
 
 // listening for a new tab opened or to a new URL typed in a tab
 chrome.tabs.onUpdated.addListener(onUpdatedTabHandler);
